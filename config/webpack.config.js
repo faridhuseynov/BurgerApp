b93111d3f34de0c6d50748c94849b3a88762e791
Farid Huseynov <faridhuseynov.eng@gmail.com>
2020-11-28 00:28:14 +0400

    Github loaded

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -469,9 +469,12 @@ module.exports = function (webpackEnv) {
               exclude: cssModuleRegex,
               use: getStyleLoaders({
                 importLoaders: 1,
+                modules:{
+                  localIdentName: '[name]__[local]__[hash:base64:5]',
+                },
                 sourceMap: isEnvProduction
-                  ? shouldUseSourceMap
-                  : isEnvDevelopment,
+                ? shouldUseSourceMap
+                : isEnvDevelopment,
               }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Auxiliary";
+
+class BurgerBuilder extends Component{
+    render() {
+        return(
+            <Aux>
+                <p>Burger</p>
+                <p>Build Control</p>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
